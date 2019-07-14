70faaf9386b44ef3e640fb6e9a6c1b49f94261b0
Anton Erokhin <er.ant14@yandex.ru>
2019-07-15 00:12:36 +0300

    Extend tslint, add input output proerties for component

--- projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.component.ts
+++ projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'gcsr-component',
   templateUrl: './google-cloud-speech-recognition.component.html',
-  styleUrls: ['./google-cloud-speech-recognition.component.scss']
+  styleUrls: ['./google-cloud-speech-recognition.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GoogleCloudSpeechRecognitionComponent implements OnInit {
 
-  constructor() { }
+  @Input() history: boolean = true;
+  @Input() checkRecognition: boolean = true;
+
+  @Output() recognitionResult: EventEmitter<any> = new EventEmitter();
+
+  constructor(private cdRef: ChangeDetectorRef) { }
 
   ngOnInit() {
   }
