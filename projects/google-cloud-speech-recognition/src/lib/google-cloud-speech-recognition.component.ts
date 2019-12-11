53faf3d9bdc8d8cd5f6287a8a9993aec75017942
Anton Erokhin <er.ant14@yandex.ru>
2019-12-12 01:13:21 +0300

    Finalize recording part and markup

--- projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.component.ts
+++ projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
 
-import { ReplaySubject, Observable, timer, from } from 'rxjs';
-import { takeWhile, tap, take, finalize } from 'rxjs/operators';
+import { ReplaySubject, Observable, timer, from, Subscription } from 'rxjs';
+import { takeUntil, tap, take } from 'rxjs/operators';
 
-declare var MediaRecorder: any;
+export const SHORT_RECORD_MAXIMUM: number = 30;
+
+export const SOUND_SOURCES: Array<ISoundSource> = [
+  {
+    key: 'micro',
+    label: 'MICROPHONE'
+  }, {
+    key: 'upload',
+    label: 'FILE UPLOAD'
+  }
+]
+
+export interface ISoundSource {
+  key: SoundSource;
+  label: string;
+}
 
 type SoundSource = 'micro' | 'upload';
 
@@ -20,22 +35,23 @@ export interface IRecognitionLanguage {
 })
 export class GoogleCloudSpeechRecognitionComponent implements OnInit, OnDestroy {
 
-  @Input() history: boolean = true;
-  @Input() checkRecognition: boolean = true;
+  // @Input() history: boolean = true;
+  // @Input() checkRecognition: boolean = true;
 
   @Output() recognitionResult: EventEmitter<any> = new EventEmitter();
 
-  private readonly availableLanguages: Array<IRecognitionLanguage>;
-
   private unsubscribe$: ReplaySubject<any> = new ReplaySubject(1);
 
-  recordingTimer: Observable<number>;
+  readonly availableSoundSources: Array<ISoundSource> = SOUND_SOURCES;
+  availableLanguages: Array<IRecognitionLanguage>;
 
-  currentSoundSource: SoundSource = 'micro';
+  recordingTimer: Subscription;
+
+  currentSoundSource: ISoundSource = this.availableSoundSources[0];
   currentLanguage: IRecognitionLanguage;
-  currentSeconds: number;
 
-  isRecording: boolean = false;
+  currentShortRecordingSeconds: number;
+  isShortRecording: boolean = false;
   languagesDropdownOpened: boolean = false;
 
   mediaRecorder: any;
@@ -70,10 +86,75 @@ export class GoogleCloudSpeechRecognitionComponent implements OnInit, OnDestroy 
   }
 
   /**
+   * @method startShortRecording
+   * Starts recording short audio
+   */
+  private startShortRecording(): void {
+    from(navigator.mediaDevices.getUserMedia({ audio: true, video: false }))
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (mediaStream: MediaStream) => {
+          this.mediaStream = mediaStream;
+          this.isShortRecording = true;
+
+          this.recordingTimer = timer(0, 1000)
+            .pipe(
+              take(SHORT_RECORD_MAXIMUM),
+              tap((second: number) => {
+                this.currentShortRecordingSeconds = second + 1;
+                this.cdRef.detectChanges();
+
+                if (second === (SHORT_RECORD_MAXIMUM - 1)) {
+                  this.stopShortRecording();
+                }
+              })
+            )
+            .subscribe();
+
+          this.mediaRecorder = new MediaRecorder(this.mediaStream);
+          this.mediaRecorder.start();
+
+          const audioChunks: Array<Blob> = [];
+
+          this.mediaRecorder.addEventListener('dataavailable', (event: BlobEvent) => {
+            audioChunks.push(event.data);
+          });
+
+          this.mediaRecorder.addEventListener('stop', () => {
+            const audioBlob = new Blob(audioChunks);
+          });
+        },
+        (err: DOMException) => console.log(err.message)
+      );
+  }
+
+  /**
+   * @method stopShortRecording
+   * Stops recording short audio
+   */
+  private stopShortRecording(): void {
+    this.currentShortRecordingSeconds = 0;
+    this.isShortRecording = false;
+    this.recordingTimer.unsubscribe();
+    this.mediaRecorder.stop();
+    this.mediaStream.getAudioTracks().forEach((track: MediaStreamTrack) => track.stop());
+    this.mediaStream = undefined;
+    this.cdRef.detectChanges();
+  }
+
+  /**
+   * @method processShortRecording
+   * Starts or stops recording short audio
+   */
+  processShortRecording(): void {
+    this.isShortRecording ? this.stopShortRecording() : this.startShortRecording();
+  }
+
+  /**
    * @method changeSoundSource
    * Change current sound source
    */
-  changeSoundSource(soundSource: SoundSource): void {
+  changeSoundSource(soundSource: ISoundSource): void {
     this.currentSoundSource = soundSource;
     this.cdRef.detectChanges();
   }
@@ -96,63 +177,4 @@ export class GoogleCloudSpeechRecognitionComponent implements OnInit, OnDestroy 
     this.cdRef.detectChanges();
   }
 
-  /**
-   * @method startRecording
-   * Starts or stops audio recording
-   */
-  startRecording(): void {
-    from(navigator.mediaDevices.getUserMedia({audio: true, video: false}))
-      .subscribe(
-        (mediaStream: MediaStream) => {
-          this.mediaStream = mediaStream;
-          this.isRecording = true;
-
-          this.recordingTimer = timer(0, 1000)
-            .pipe(
-              take(30),
-              takeWhile(() => !!this.recordingTimer),
-              tap((index: number) => {
-                this.currentSeconds = index + 1;
-                this.cdRef.detectChanges();
-                // return index + 1 + '';
-              }),
-              finalize(() => {
-                this.stopRecording();
-              })
-            );
-          this.recordingTimer.subscribe();
-
-
-          this.cdRef.detectChanges();
-
-          this.mediaRecorder = new MediaRecorder(mediaStream);
-          this.mediaRecorder.start();
-
-          const audioChunks: Array<Blob> = [];
-
-          this.mediaRecorder.addEventListener('dataavailable', (event: any) => audioChunks.push(event.data));
-
-          this.mediaRecorder.addEventListener('stop', () => {
-            const audioBlob = new Blob(audioChunks);
-          });
-        },
-        (err: DOMException) => console.log(err.message)
-      );
-  }
-
-  /**
-   * @method stopRecording
-   * Stops recording
-   */
-  stopRecording(): void {
-    console.log('hi');
-    this.currentSeconds = 0;
-    this.isRecording = false;
-    this.recordingTimer = null;
-    this.recordingTimer
-    this.mediaRecorder.stop();
-    this.mediaStream.getAudioTracks().forEach((track: MediaStreamTrack) => track.stop());
-    this.mediaStream = undefined;
-    this.cdRef.detectChanges();
-  }
 }
