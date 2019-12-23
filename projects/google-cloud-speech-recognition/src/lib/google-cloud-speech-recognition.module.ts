c7abcc7a03a7f25243df2135ab47a8a3d055146a
Anton Erokhin <er.ant14@yandex.ru>
2019-12-24 00:49:02 +0300

    Extend module to support configs

--- projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.module.ts
+++ projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.module.ts
@@ -1,17 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GoogleCloudSpeechRecognitionComponent } from './google-cloud-speech-recognition.component';
+
+import { GoogleCloudSpeechRecognitionService } from './google-cloud-speech-recognition.service';
+
+import { IGCSRServiceConfig } from './google-cloud-speech-recognition.models';
+
+import { GSCRConfig } from './google-cloud-speech-recognition.tokens';
 
 @NgModule({
   declarations: [
     GoogleCloudSpeechRecognitionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   exports: [
     GoogleCloudSpeechRecognitionComponent
   ]
 })
-export class GoogleCloudSpeechRecognitionModule { }
+export class GoogleCloudSpeechRecognitionModule {
+
+  static forRoot(config: IGCSRServiceConfig): ModuleWithProviders {
+    return {
+      ngModule: GoogleCloudSpeechRecognitionModule,
+      providers: [
+        GoogleCloudSpeechRecognitionService,
+        { provide: GSCRConfig, useValue: config }
+      ]
+    };
+  }
+}
