0a4f079b08c5fed7e184807e4387fcd4eef881c3
Anton Erokhin <er.ant14@yandex.ru>
2019-12-24 16:49:07 +0300

    Group models

--- projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.models.ts
+++ projects/google-cloud-speech-recognition/src/lib/google-cloud-speech-recognition.models.ts
@@ -1,48 +1,12 @@
-type SoundSource = 'microrecord' | 'stream';
-
-export interface IGCSRServiceConfig {
-  googleKey: string;
-}
-
-export interface ISoundSource {
-  key: SoundSource;
-  label: string;
-}
-
+/* Public Models */
 export interface IProcessError {
   name: 'MEDIA_ERROR' | 'GCSR_ERROR' | 'COMPONENT_ERRORS';
   message: string;
 }
 
-export interface IGCRSError {
-  code: number;
-  message: string;
-  status: string;
-}
-
-export interface IRecognitionLanguage {
-  key: string;
-  label: string;
-}
-
-export interface IGCSRRequestBody {
-  config: IGCSRConfigs;
-  audio: {
-    content: string;
-  };
-}
-
-export interface IGCSRResponse {
-  results: Array<IGCSRResult>;
-}
-
-export interface IGCSRResult {
-  alternatives: Array<IRecognitionResults>;
-}
-
-export interface IRecognitionResults {
-  confidence: number;
-  transcript: string;
+/* Configs */
+export interface IGCSRServiceConfig {
+  googleKey: string;
 }
 
 export interface IGCSRConfigs {
@@ -74,4 +38,110 @@ export interface IGCSRConfigs {
   sampleRateHertz: number;
   speechContexts?: Array<any>;
   useEnhanced?: boolean;
+}
+
+export interface IRecordRTCConfigs {
+  type: 'audio' | 'video' | 'canvas' | 'gif';
+  // audio/webm
+  // video/webm;codecs=vp9
+  // video/webm;codecs=vp8
+  // video/webm;codecs=h264
+  // video/x-matroska;codecs=avc1
+  // video/mpeg -- NOT supported by any browser, yet
+  // video/mp4  -- NOT supported by any browser, yet
+  // audio/wav
+  // audio/ogg  -- ONLY Firefox
+  // demo: simple-demos/isTypeSupported.html
+  mimeType: string;
+  // MediaStreamRecorder, StereoAudioRecorder, WebAssemblyRecorder
+  // CanvasRecorder, GifRecorder, WhammyRecorder
+  recorderType: any;
+  // disable logs
+  disableLogs: boolean;
+  // get intervals based blobs
+  // value in milliseconds
+  timeSlice: number;
+  // requires timeSlice above
+  // returns blob via callback function
+  ondataavailable: (blob) => {};
+  // auto stop recording if camera stops
+  checkForInactiveTracks: boolean,
+  // requires timeSlice above
+  onTimeStamp: (timestamp) => {};
+  // both for audio and video tracks
+  bitsPerSecond: number;
+  // only for audio track
+  audioBitsPerSecond: number;
+  // only for video track
+  videoBitsPerSecond: number;
+  // used by CanvasRecorder and WhammyRecorder
+  // it is kind of a "frameRate"
+  frameInterval: number;
+  // if you are recording multiple streams into single file
+  // this helps you see what is being recorded
+  previewStream: (stream) => {};
+  // used by CanvasRecorder and WhammyRecorder
+  // you can pass {width:640, height: 480} as well
+  video: HTMLVideoElement;
+  // used by CanvasRecorder and WhammyRecorder
+  canvas: object;
+  // used by StereoAudioRecorder
+  // the range 22050 to 96000.
+  sampleRate: number;
+  // used by StereoAudioRecorder
+  // the range 22050 to 96000.
+  // let us force 16khz recording:
+  desiredSampRate: number;
+  // used by StereoAudioRecorder
+  // Legal values are (256, 512, 1024, 2048, 4096, 8192, 16384).
+  bufferSize: number;
+  // used by StereoAudioRecorder
+  // 1 or 2
+  numberOfAudioChannels: number;
+  // used by WebAssemblyRecorder
+  frameRate: number;
+  // used by WebAssemblyRecorder
+  bitrate: number;
+  // used by MultiStreamRecorder - to access HTMLCanvasElement
+  elementClass: string;
+}
+
+/* GCSR Requests Responses bodies */
+export interface IGCSRRequestBody {
+  config: IGCSRConfigs;
+  audio: {
+    content: string;
+  };
+}
+
+export interface IGCSRResponse {
+  results: Array<IGCSRResult>;
+}
+
+export interface IGCSRResult {
+  alternatives: Array<IRecognitionResults>;
+}
+
+export interface IRecognitionResults {
+  confidence: number;
+  transcript: string;
+}
+
+export interface IGCRSError {
+  code: number;
+  message: string;
+  status: string;
+}
+
+/* Sound Sources part and etc. */
+type SoundSource = 'microrecord' | 'stream';
+
+export interface ISoundSource {
+  key: SoundSource;
+  label: string;
+}
+
+export interface IRecognitionLanguage {
+  key: string;
+  label: string;
 }
