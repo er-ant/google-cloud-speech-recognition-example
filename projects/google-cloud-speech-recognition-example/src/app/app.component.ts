0a5dfc96a9cfd9a6019d78e59055f4e33673e625
Anton Erokhin <er.ant14@yandex.ru>
2019-12-24 13:26:54 +0300

    Integrate app component with results

--- projects/google-cloud-speech-recognition-example/src/app/app.component.ts
+++ projects/google-cloud-speech-recognition-example/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { IRecognitionResults } from 'projects/google-cloud-speech-recognition/src/public-api';
+
 @Component({
   selector: 'gcsr-example-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'google-cloud-speech-recognition-example';
+
+  gcsrResultsGroups: Array<Array<IRecognitionResults>> = [];
+
+  gcsrResultsHandler($event: Array<IRecognitionResults>): void {
+    this.gcsrResultsGroups.unshift($event);
+  }
 }
